6ff19010ab641e57e6e92dd7402810e3c29df549
iNanzo <newtonhoang99@gmail.com>
2025-03-11 13:30:26 -0700

    updated attack

--- src/Creature.java
+++ src/Creature.java
@@ -1,11 +1,13 @@
 // Newton Hoang
 
+import java.util.Random;
+
 public abstract class Creature {
     private int hitPoints;
     private int attackPoints;
     private boolean alive;
     private String creatureType;
-    private int gold; // Add gold property
+    private int gold;
 
     public Creature(int hitPoints, int attackPoints, String creatureType) {
         this.hitPoints = hitPoints;
@@ -35,8 +37,29 @@ public abstract class Creature {
         return gold;
     }
 
-    public void setGold(int gold) { // Fix: Add setGold() method
+    public void setGold(int gold) {
         this.gold = gold;
+    }
+
+    // Generates random attack damage (between 1 and attackPoints)
+    public int attacks() {
+        Random rand = new Random();
+        return rand.nextInt(attackPoints) + 1;
+    }
+
+    // Calls assessDamage() to update hitPoints and return alive status
+    public boolean defends(int damage) {
+        assessDamage(damage);
+        return alive;
+    }
+
+    // New assessDamage() method that ensures HP does not go below zero
+    private void assessDamage(int damage) {
+        hitPoints -= damage;
+        if (hitPoints <= 0) {
+            hitPoints = 0;
+            alive = false;
+        }
     }
 
     public abstract String toString();
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,45 @@
 
 public class Main {
     public static void main(String[] args) {
-        Hero hero = new Hero("Hero", 20, 5, 0, "Conan", "Barbarian");
-        Monster monster = new Monster("Dragon", 40, 10, 200, "Fierce");
+        Hero hero = new Hero("Hero", 120, 10, 0, "Rowan Lonestag", "Paladin");
+        Monster monster = new Monster("Dragon", 400, 15, 200, "Fierce");
 
+        System.out.println("⚔️ The battle begins! ⚔️");
         System.out.println(hero);
         System.out.println(monster);
+        System.out.println("-------------------------------------");
+
+        int round = 1;
+
+        while (hero.isAlive() && monster.isAlive()) {
+            System.out.println("🌀 Round " + round + " 🌀");
+
+            // Hero attacks
+            int heroDamage = hero.attacks();
+            System.out.println(hero.getCreatureType() + " attacks for " + heroDamage + " damage!");
+            boolean monsterStillAlive = monster.defends(heroDamage);
+            System.out.println(monster);
+
+            if (!monsterStillAlive) {
+                System.out.println("🏆 The hero has defeated the monster!");
+                break;
+            }
+
+            // Monster attacks
+            int monsterDamage = monster.attacks();
+            System.out.println(monster.getCreatureType() + " attacks for " + monsterDamage + " damage!");
+            boolean heroStillAlive = hero.defends(monsterDamage);
+            System.out.println(hero);
+
+            if (!heroStillAlive) {
+                System.out.println("💀 The monster has defeated the hero!");
+                break;
+            }
+
+            System.out.println("-------------------------------------");
+            round++;
+        }
+
+        System.out.println("🏁 The battle is over! 🏁");
     }
 }
